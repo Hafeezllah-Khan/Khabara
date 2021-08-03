a0df90d72c465330c5e29b943bfef46f41d65202
Hafeezullah-Khan <hafeeziiui3@gmail.com>
2021-08-03 11:53:38 +0500

    initial commit

---
+++ app/src/main/java/com/example/khabrav1/ChatActivity.java
@@ -0,0 +1,124 @@
+package com.example.khabrav1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.khabrav1.databinding.ActivityChatBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
+
+    MessagesAdapter adapter;
+    ArrayList<Message> messages;
+
+    String senderRoom, receiverRoom;
+
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        messages = new ArrayList<>();
+        adapter = new MessagesAdapter(this, messages, senderRoom, receiverRoom);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(adapter);
+
+        String name = getIntent().getStringExtra("name");
+        //String uid = getIntent().getStringExtra("uid");
+        String receiverUid = getIntent().getStringExtra("uid");
+        //String currentUid = FirebaseAuth.getInstance().getUid();
+        String senderUid = FirebaseAuth.getInstance().getUid();
+
+        senderRoom = senderUid + receiverUid;
+        receiverRoom = receiverUid + senderUid;
+
+        database = FirebaseDatabase.getInstance();
+
+        database.getReference().child("chats")
+                .child(senderRoom)
+                .child("messages")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                        messages.clear();// clear messages arraylist
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()){
+                            Message message = snapshot1.getValue(Message.class);
+                            message.setMessageId(snapshot1.getKey());
+                            messages.add(message);
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
+
+        binding.sendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageTxt = binding.messageBox.getText().toString();
+
+                Date date = new Date();
+                Message message = new Message(messageTxt, senderUid, date.getTime());
+                binding.messageBox.setText("");
+
+                String randomKey = database.getReference().push().getKey();
+
+                HashMap<String, Object> lastMsgObj = new HashMap<>();
+                lastMsgObj.put("lastMsg", message.getMessage());
+                lastMsgObj.put("lastMsgTime", date.getTime());
+
+                database.getReference().child("chats").child(senderRoom).updateChildren(lastMsgObj);
+                database.getReference().child("chats").child(receiverRoom).updateChildren(lastMsgObj);
+
+                database.getReference().child("chats")
+                        .child(senderRoom)
+                        .child("messages")
+                        //.push()
+                        .child(randomKey)
+                        .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        database.getReference().child("chats")
+                                .child(receiverRoom)
+                                .child("messages")
+                                //.push()
+                                .child(randomKey)
+                                .setValue(message).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+
+                            }
+                        });
+
+
+                    }
+                });
+            }
+        });
+
+        getSupportActionBar().setTitle(name);
+    }
+}
---
+++ app/src/main/java/com/example/khabrav1/UsersAdapter.java
@@ -0,0 +1,111 @@
+package com.example.khabrav1;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+
+import android.support.annotation.NonNull;
+
+//import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.khabrav1.databinding.RowConversationBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UsersViewHolder> {
+
+    Context context;
+    ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users){
+        this.context = context;
+        this.users = users;
+    }
+
+    @androidx.annotation.NonNull
+
+    @Override
+    public UsersViewHolder onCreateViewHolder(@androidx.annotation.NonNull  ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_conversation,parent,false);
+
+        return new UsersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@androidx.annotation.NonNull  UsersAdapter.UsersViewHolder holder, int position) {
+
+        User user = users.get(position);
+
+        String senderId = FirebaseAuth.getInstance().getUid();
+
+        String senderRoom = senderId + user.getUid();
+
+        FirebaseDatabase.getInstance().getReference()
+                .child("chats")
+                .child(senderRoom)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@androidx.annotation.NonNull @NotNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            String lastMsg = snapshot.child("lastMsg").getValue(String.class);
+                            long time = snapshot.child("lastMsgTime").getValue(Long.class);
+
+                            holder.binding.lastMsg.setText(lastMsg);
+                        }else {
+                            holder.binding.lastMsg.setText("Tap to chat");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@androidx.annotation.NonNull @NotNull DatabaseError error) {
+
+                    }
+                });
+
+
+        holder.binding.username.setText(user.getName());
+
+        //holder.binding.profile.
+        Glide.with(context).load(user.getProfileImage())
+                .placeholder(R.drawable.avatar)//if we get no Image
+                .into(holder.binding.profile);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ChatActivity.class);
+                intent.putExtra("name", user.getName());
+                intent.putExtra("image", user.getProfileImage());
+                intent.putExtra("uid", user.getUid());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UsersViewHolder extends RecyclerView.ViewHolder{
+
+        RowConversationBinding binding;
+
+        public UsersViewHolder(@NonNull  View itemView) {
+            super(itemView);
+            binding = RowConversationBinding.bind(itemView);
+        }
+    }
+}
